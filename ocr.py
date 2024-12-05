3ebb4bbe507d635c985303e317c30115fc03bc29
Your Name <your.email@example.com>
2024-12-05 01:34:56 -0300

    feat: removed useless comments

--- ocr.py
+++ ocr.py
@@ -1,12 +1,9 @@
 import pytesseract
 from PIL import Image
 
-# Path to the image inside the container (same directory as ocr.py)
-img_path = "testeocr3.jpg"  # The image is directly in the container's /app directory
+img_path = "testeocr3.jpg"
 
-# Open the image
 img = Image.open(img_path)
 
-# Run OCR on the image
 text = pytesseract.image_to_string(img)
 print(f"Extracted text: {text}")
